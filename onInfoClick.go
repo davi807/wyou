b9a2be7652499f4a37387074361e5a996843fa62
davi807 <davi807plus@yandex.ru>
2021-07-28 03:28:13 +0400

    video thumbnail image and title and duration

--- onInfoClick.go
+++ onInfoClick.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"net/url"
 	"os"
 	"os/exec"
-	"sort"
-	"strings"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -16,20 +13,10 @@ import (
 )
 
 var currentJsonFile string
-var jsonOutput = []byte{}
+var video = videoData{}
 
 func onInfoClick() {
-	/*
-		go func() {
-			resp, _ := http.Get("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Pia31MHkdochSVtOiP2P_k8rjBPtGl0Uvg&usqp=CAU")
-			img := canvas.NewImageFromReader(resp.Body, "afadas")
-
-			img.SetMinSize(fyne.Size{320, 240})
-			icont := container.NewGridWrap(fyne.Size{320, 240}, img)
-
-			content.Add(icont)
-		}()
-	*/
+	result.Hide()
 
 	progress.Show()
 	defer progress.Hide()
@@ -49,48 +36,11 @@ func onInfoClick() {
 		return
 	}
 
-	video := videoData{}
 	json.Unmarshal(jsonOutput, &video)
 
 	currentJsonFile = os.TempDir() + "/" + video.Id + time.Now().Format("150405.000") + ".tmp"
 	tmpF, _ := os.Create(currentJsonFile)
 	tmpF.Write(jsonOutput)
-}
 
-func getVideoThumbnail(vd videoData) thumbnail {
-
-	exts := []string{"jpg", "png", "bmp", "gif"}
-
-	thumbMainUrl := thumbnail{Url: vd.ThumbnailURL}
-
-	thumbs := append([]thumbnail{thumbMainUrl}, vd.Thumbnails...)
-
-	sort.Slice(
-		thumbs,
-		func(i, j int) bool { return thumbs[j].Width < thumbs[i].Width },
-	)
-
-	for _, thumb := range thumbs {
-		thurl, err := url.Parse(thumb.Url)
-
-		if err != nil {
-			continue
-		}
-
-		imageExt := strings.Split(thurl.Path, ".")
-		if len(imageExt) == 0 {
-			continue
-		}
-
-		ext := strings.ToLower(imageExt[len(imageExt)-1])
-
-		for _, ex := range exts {
-			if ext == ex {
-				return thumb
-			}
-		}
-
-	}
-
-	return thumbnail{}
+	go createVideoPage()
 }
--- pages.go
+++ pages.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"sort"
+	"strings"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
@@ -16,13 +23,9 @@ var urlBar *widget.Entry
 var searchButton *widget.Button
 
 var progress fyne.Widget
-var resultContainer fyne.Widget
 
-func SetWindowContent(cnt *fyne.Container) {
-	window.SetContent(
-		container.NewGridWrap(fyne.Size{Width: APP_WIDTH, Height: APP_HEIGHT}, cnt),
-	)
-}
+var resultContainer container.Scroll
+var result *fyne.Container
 
 /* Pages */
 
@@ -41,12 +44,127 @@ func initStartPage() {
 	progress = widget.NewProgressBarInfinite()
 	progress.Hide()
 
+	result = container.NewVBox()
+
+	resultContainer := container.NewVScroll(result)
+	resultContainer.SetMinSize(fyne.NewSize(0, 320))
+
 	content = container.NewVBox(
 		titleInfo,
 		urlBar,
 		searchButton,
 		progress,
+		resultContainer,
 	)
 
 	SetWindowContent(content)
+}
+
+func createVideoPage() {
+	result.Objects = []fyne.CanvasObject{}
+
+	topBar := container.NewGridWithColumns(2)
+	imageContainer := container.NewGridWrap(fyne.NewSize(THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT))
+	infoContainer := container.NewVBox()
+
+	title := widget.NewLabel(video.Title)
+	title.Alignment = fyne.TextAlignCenter
+	title.Wrapping = fyne.TextWrapWord
+	title.TextStyle.Bold = true
+	infoContainer.Add(title)
+
+	infoContainer.Add(widget.NewLabel("Duration: " + formatDuration(video.Duration)))
+	infoContainer.Add(widget.NewLabel("Provider: " + video.Extractor))
+
+	go func() {
+		resp, _ := http.Get(getVideoThumbnail(video).Url)
+		img := canvas.NewImageFromReader(resp.Body, video.Id)
+
+		img.SetMinSize(fyne.NewSize(THUMBNAIL_WIDTH-5, THUMBNAIL_HEIGHT-5))
+
+		imageContainer.Add(img)
+	}()
+
+	topBar.Add(imageContainer)
+	topBar.Add(infoContainer)
+
+	result.Add(topBar)
+	result.Show()
+}
+
+func getVideoThumbnail(vd videoData) thumbnail {
+
+	exts := []string{"jpg", "png", "bmp", "gif"}
+
+	thumbMainUrl := thumbnail{Url: vd.ThumbnailURL}
+
+	thumbs := append([]thumbnail{thumbMainUrl}, vd.Thumbnails...)
+
+	sort.Slice(
+		thumbs,
+		func(i, j int) bool { return thumbs[j].Width < thumbs[i].Width },
+	)
+
+	for _, thumb := range thumbs {
+		thurl, err := url.Parse(thumb.Url)
+
+		if err != nil {
+			continue
+		}
+
+		imageExt := strings.Split(thurl.Path, ".")
+		if len(imageExt) == 0 {
+			continue
+		}
+
+		ext := strings.ToLower(imageExt[len(imageExt)-1])
+
+		for _, ex := range exts {
+			if ext == ex {
+				if video.Extractor == "Youtube" {
+					thumb.Url = strings.Replace(thumb.Url, thurl.RawQuery, "", 1)
+				}
+				return thumb
+			}
+		}
+
+	}
+
+	return thumbnail{Url: THUMBNAIL_DEFAULT_URL}
+}
+
+// ===============
+// ===============
+// ===============
+
+/* Content set on window */
+
+func SetWindowContent(cnt *fyne.Container) {
+	window.SetContent(
+		container.NewGridWrap(fyne.NewSize(APP_WIDTH, APP_HEIGHT), cnt),
+	)
+}
+
+/* format video duration */
+
+func formatDuration(d int) string {
+	// lib return video duration in seconds,
+	//convert go's  nanosecond format to seconds
+
+	res := ""
+	spr := ":"
+
+	hours := d / 3600
+	rems := d % 3600
+	minutes := rems / 60
+	seconds := d % 60
+
+	if hours > 0 {
+		res += fmt.Sprintf("%d", hours) + spr
+	}
+
+	res += fmt.Sprintf("%2d", minutes) + spr
+	res += fmt.Sprintf("%2d", seconds)
+
+	return res
 }
