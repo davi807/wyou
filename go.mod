b6025703f07faa345123841ac084411ea2a61415
davi807 <davi807plus@yandex.ru>
2021-08-07 01:36:28 +0400

    initial commit for new webview based engine

--- go.mod
+++ go.mod
@@ -3,6 +3,5 @@ module wyou
 go 1.15
 
 require (
-	fyne.io/fyne v1.4.3
-	fyne.io/fyne/v2 v2.0.3
+	github.com/webview/webview v0.0.0-20210330151455-f540d88dde4e
 )
--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
+import "github.com/webview/webview"
+
+var window webview.WebView
+
 func main() {
-	initStartPage()
-	window.ShowAndRun()
+	debug := true
+
+	window := webview.New(debug)
+	defer window.Destroy()
+
+	window.Bind("load", loader)
+	window.SetTitle(APP_TITLE)
+	window.SetSize(APP_WIDTH, APP_HEIGHT, webview.HintFixed)
+
+	window.Navigate(initHTML())
+
+	window.Run()
 }
