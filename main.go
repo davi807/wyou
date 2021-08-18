63fb623d03d585ff4dcc3def07427f663fb7cd56
davi807 <davi807plus@yandex.ru>
2021-08-18 22:38:04 +0400

    gitignore add windows binary add browser auto open default browser mini fix in css

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os/exec"
+	"time"
+)
 
 func main() {
 	prepareServer()
 	makeServerHnadlers()
 
-	println("Server working on\n" + "http://" + address + ":" + port)
+	url := "http://" + address + ":" + port
+	println("Server working on\n" + url)
+	println("Opening browser...")
+
+	go func() {
+		time.Sleep(500 * time.Microsecond)
+		exec.Command(DEFAULT_BROWSER, url).Output()
+		println("\nPLEASE NOT CLOSE THIS PROGRAM")
+	}()
+
 	http.ListenAndServe(address+":"+port, nil)
 }
--- values.go
+++ values.go
@@ -8,16 +8,14 @@ import (
 var YT_NAME = "youtube-dl"
 var FF_NAME = "ffmpeg"
 
+var DEFAULT_BROWSER = "xdg-open"
+
 func init() {
 	if runtime.GOOS == "windows" {
 		YT_NAME = filepath.Join("bin", YT_NAME)
 		FF_NAME = filepath.Join("bin", FF_NAME)
+		DEFAULT_BROWSER = "explorer"
 	}
 }
-
-// const THUMBNAIL_WIDTH = 336
-// const THUMBNAIL_HEIGHT = 188
-
-// const THUMBNAIL_DEFAULT_URL = "https://via.placeholder.com/336x188.png?text=Image+not+found"
 
 const OUTPUT_FILE_NAME = "%(title)s.%(ext)s"
